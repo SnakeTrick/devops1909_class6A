a244c1c7e0b7354b6968dfe34ccfb14e33f2bf02
SnakeTrick <nxmshop@gmail.com>
2024-11-21 16:34:28 +0200

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,29 @@
+a = 4
+b = "aviel"
+c = True
+d = ["aviel", "Buskila", 34, True]
+e = {"fname": "aviel",
+     "lname": "buskila",
+     "age": 34}
+f = dict(fname="aviel",lanme="buskila",age=34)
+list_of_people = [e,f]
+print(d[2])
+print(a)
+print("name is " + b)
+print(list_of_people)
+r = 4 + 4
+s = "aviel" + "buskila"
+t = "aviel" * 4
+u = "aviel" + str(4)
+v = int(5.4)
+s = "aviel" + "buskila"
+fname = "aviel"
+lname = "buskila"
+first = fname + lname
+second = "{} {} ".format(lname, fname)
+third = f"{fname} {lname}"
+forth = "%s %s" % (fname , lname)
+print(t)
+print(u)
+print(v)
+print(first,second,third,forth)
